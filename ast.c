b77c146b8c5cbf510c27e5cffe6657ad08cf419a
Mayank Jain <31382541+mayank3012jain@users.noreply.github.com>
2020-03-29 20:44:49 +0530

    ast_statement_rules

--- ast.c
+++ ast.c
@@ -18,7 +18,7 @@ ASTnode* allocateAstNode(int label, ASTnode* parent, ASTnode* firstChild, ASTnod
 ASTnode* populateAST(ptree_node* root, ASTnode* parent){
 
     int rule = root->rule;
-    ASTnode* syn, inh, current;
+    ASTnode *syn, *inh, *current;//check if correct
       
     switch(rule-1){
         
@@ -379,6 +379,184 @@ ASTnode* populateAST(ptree_node* root, ASTnode* parent){
             }
             break;
 
+        case 8: //module -> DEF MODULE ID ENDDEF TAKES INPUT SQBO inputplist SQBC SEMICOL ret moduledef
+            current = allocateAstNode(MODULE_NODE, parent, NULL, NULL, root);
+            int arr[] = {2,7,10,11};
+
+            if(current!=NULL){
+                int index = 0;
+                ASTnode* temp = allocateAstNode(ID_NODE, current, NULL, NULL, root->children[arr[index]]);
+                ASTnode* temp2 = current;
+                while(index < 4){
+                    while(index < 4 && temp == NULL){
+                        index++;
+                        temp = populateAST(root->children[arr[index]], current);
+                    }
+                    if(index == 4){
+                        break;
+                    }
+                    else{
+                        if(temp2 == current){
+                            current->firstChild = temp;
+                            temp2 = temp;
+                        }else{
+                            temp2->sibling = temp;
+                            temp2 = temp;
+                        }
+                    }
+                }
+            }
+            break;
+
+        case 20://datatype -> ARRAY SQBO rangearrays SQBC OF type
+            current = allocateAstNode(DATATYPE_ARRAY_NODE, parent, NULL, NULL, root);
+            current->firstChild =  populateAST(root->children[2], current);
+            current->firstChild->sibling = populateAST(root->children[5], current);
+            break;
+
+        case 21: //rangearrays -> ind RANGEOP ind1
+            current = allocateAstNode(RANGEARRAYS_NODE, parent, NULL, NULL, root);
+            current->firstChild = populateAST(root->children[0], current);
+            current->firstChild->sibling = populateAST(root->children[2], current);
+            break;
+            
+        case 53: // modulereusestmt -> optional USE MODULE ID WITH PARAMETERS idlist SEMICOL
+            current = allocateAstNode(MODEULEREUSESTMT_NODE, parent, NULL, NULL, root);
+            int arr[] = {0,3,6};
+
+            if(current!=NULL){
+                int index = 0;
+                ASTnode* temp = populateAST(optional, current, NULL, NULL, root->children[arr[index]]);
+                ASTnode* temp2 = current;
+                while(index < 3){
+                    while(index < 3 && temp == NULL){
+                        index++;
+                        if(index == 1){
+                            temp = allocateAstNode(ID_NODE, current, NULL, NULL, root->children[arr[index]]);
+                        }
+                        else{
+                            temp = populateAST(root->children[arr[index]], current);
+                        }
+                    }
+                    if(index == 3){
+                        break;
+                    }
+                    else{
+                        if(temp2 == current){
+                            current->firstChild = temp;
+                            temp2 = temp;
+                        }else{
+                            temp2->sibling = temp;
+                            temp2 = temp;
+                        }
+                    }
+                }
+            }
+            break;
+            
+        
+        case 33: //iostmt -> GET_VALUE BO ID BC SEMICOL
+            current = allocateAstNode(GET_STMT_NODE, parent, NULL, NULL, root);
+            current->firstChild = allocateAstNode(ID_NODE, current, NULL, NULL, root->children[2]);
+            break;
+
+        case 34: //iostmt -> PRINT BO var BC SEMICOL
+            current = allocateAstNode(PRINT_STMT_NODE, parent, NULL, NULL, root);
+            current->firstChild = populateAST(root->children[2], current);
+            break;
+
+        case 37: //varidnum -> ID whichid
+            current = allocateAstNode(VARIDNUM_NODE, parent, NULL, NULL, root);
+            current->firstChild = allocateAstNode(ID_NODE, current, NULL, NULL, root->children[0]);
+            ASTnode* temp = populateAST(root->children[1], current);
+            if (temp != NULL)
+                current->firstChild->sibling = temp;
+            break; 
+            
+        //assignment statement rules 46 to 50, assignstmt_node will have its 2 children as lhs and rhs of assignop
+        
+        case 46: //assignstmt -> ID whichstmt
+            current = populateAST(root->children[1], current);
+            if(current->label == ASSIGNOP_NODE){
+                inh = allocateAstNode(ID_NODE, NULL, NULL, NULL, root->children[0]);
+                syn = current->firstChild;
+                current->firstChild = inh;
+                inh->parent = current;
+                current->firstChild->sibling = syn;
+            }else if(current->label == ASSIGNOP_ARRAY_NODE){
+                inh = allocateAstNode(ID_NODE, NULL, NULL, NULL, root->children[0]);
+                syn = current->firstChild;
+                current->firstChild = allocateAstNode(ID_ARRAY_NODE, current, inh, syn->sibling, NULL);
+                inh->parent = current->firstChild;
+                inh->sibling = syn;
+                syn->sibling = NULL;
+                syn->parent = current->firstChild;
+            }else{
+                printf("Error assignop");
+            }
+            break;
+            
+        case 47: //whichstmt -> lvalueidstmt
+            current = populateAST(root->children[0],parent);
+            break;
+
+        case 48: //whichstmt -> lvaluearrstmt
+            current = populateAST(root->children[0],parent);
+            break;
+
+        case 49: //lvalueidstmt -> ASSIGNOP expression SEMICOL
+            current = allocateAstNode(ASSIGNOP_NODE, parent, NULL, NULL, root->children[0]);
+            current->firstChild = populateAST(root->children[1], current);
+            break;
+        
+        case 50: //lvaluearrstmt -> SQBO ind SQBC ASSIGNOP expression SEMICOL
+            current = allocateAstNode(ASSIGNOP_ARRAY_NODE, parent, NULL, NULL, root->children[0]);
+            current->firstChild = populateAST(root->children[1], current);
+            current->firstChild->sibling = populateAST(root->children[4], current);
+            break;
+
+
+        case 61: //u -> op1 newnt
+            current = allocateAstNode(U_NODE, parent, NULL, NULL, root);
+            current->firstChild = allocateAstNode(OP1_NODE, current, NULL, NULL, root->children[0]);
+            current->firstChild->sibling = populateAST(root->children[1], current);
+            break;
+
+        case 91: //declarestmt -> DECLARE idlist COLON datatype SEMICOL
+            current = allocateAstNode(DECLARESTMT_NODE, parent, NULL, root);
+
+            current->firstChild = populateAST(root->children[3],current);
+            current->firstChild->sibling = populateAST(root->children[1],current);
+            break;
+            
+        case 92: //conditionalstmt -> SWITCH BO ID BC START casestmts dflt END
+            current = allocateAstNode(CONDITIONALSTMT_NODE, parent, NULL, NULL, root);
+            current->firstChild = allocateAstNode(ID_NODE, current, NULL, NULL, root->children[2]);
+            current->firstChild->sibling = populateAST(root->children[5], current);
+            ASTNode* temp = populateAST(root->children[6], current);
+            if (temp != NULL)
+                current->firstChild->sibling->sibling = temp;
+            
+            break;
+    
+        case 101: //iterativestmt -> FOR BO ID IN range BC START statements END
+            current = allocateAstNode(FORITERATIVESTMT_NODE, parent, NULL, NULL, root);
+            current->firstChild = allocateAstNode(ID_NODE, current, NULL, NULL, root->children[2]);
+            current->firstChild->sibling = populateAST(root->children[4], current);
+            current->firstChild->sibling->sibling = populateAST(root->children[7], current);
+            break;
+
+        case 102: //iterativestmt -> WHILE BO arithmeticorbooleanexpr BC START statements END
+            current = allocateAstNode(WHILEITERATIVESTMT_NODE, parent, NULL, NULL, root);
+            current->firstChild = populateAST(root->children[1], current);
+            current->firstChild->sibling = populateAST(root->children[2], current);
+            break; 
+    
+        case 103: //range -> NUM RANGEOP NUM1
+            current = allocateAstNode(RANGE_NODE, parent, NULL, NULL, root);
+            current->firstChild = allocateAstNode(NUM_NODE, current, NULL, NULL, root->children[0]);
+            current->firstChild->sibling = allocateAstNode(NUM_NODE, current, NULL, NULL, root->children[2]);
+            break;
         
     }
        
